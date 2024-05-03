f8feb688f5f5bf374d2d4a77dc788b4985192e95
huycuong23 <cuongcuonghuy@gmail.com>
2024-05-03 11:35:40 +0700

    add root

--- tree.py
+++ tree.py
@@ -5,52 +5,62 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import csv
 
-# Open the CSV file
-file = str(input("nhap ten file: "))
+# Nhập tên file CSV
+file = str(input("Nhập tên file: "))
+
+# Đọc dữ liệu từ file CSV
 with open(file, newline='') as csvfile:
-    # Create a CSV reader object with comma as the delimiter
     csvreader = csv.reader(csvfile, delimiter=',')
-    # Initialize an empty list to store the rows
     output = []
-    # Iterate over each row in the CSV file
     for row in csvreader:
-        # Append the row to the output list
         output.append(row)
 
-# Print the output
-
-# Tạo DataFrame từ dữ liệu gốc
+# Tạo DataFrame
 df = pd.DataFrame(output)
 
-# Thay thế None bằng chuỗi trống
+# Xử lý giá trị None
 df = df.applymap(lambda x: '' if x is None else x)
 
-# Mã hoá dữ liệu
+# Mã hóa dữ liệu
 encoded_df = pd.get_dummies(df.apply(lambda x: ','.join(x), axis=1).str.get_dummies(','))
 
-minsupportInput =float(input("nhap min support: "))
+# Nhập giá trị min_support
+minsupportInput = float(input("Nhập min support: "))
+
 # Tìm tập phổ biến
 frequent_itemsets = fpgrowth(encoded_df, min_support=minsupportInput, use_colnames=True)
 
-# Vẽ cây với trọng số là tần suất xuất hiện
+# Thêm cột 'length' để lưu độ dài của mỗi itemset
+frequent_itemsets['length'] = frequent_itemsets['itemsets'].apply(lambda x: len(x))
+
+# Khởi tạo đồ thị
 G = nx.Graph()
-for index, row in frequent_itemsets.iterrows():
-    items = list(row['itemsets'])  # Chuyển đổi thành danh sách
+
+# Thêm node gốc
+G.add_node("Root")
+
+# Kết nối node gốc với tập phổ biến có độ dài 1
+for index, row in frequent_itemsets[frequent_itemsets['length'] == 1].iterrows():
+    G.add_edge("Root", row['itemsets'], weight=row['support'])
+
+# Thêm các cạnh và trọng số cho tập phổ biến có độ dài lớn hơn 1
+for index, row in frequent_itemsets[frequent_itemsets['length'] > 1].iterrows():
+    items = list(row['itemsets'])
     for i in range(len(items) - 1):
         item1 = items[i]
         item2 = items[i+1]
         if G.has_edge(item1, item2):
-            # Tăng trọng số nếu cạnh đã tồn tại
-            G[item1][item2]['weight'] += 1
+            G[item1][item2]['weight'] += row['support']
         else:
-            # Thêm cạnh mới với trọng số 1
-            G.add_edge(item1, item2, weight=1)
+            G.add_edge(item1, item2, weight=row['support'])
 
-pos = nx.spring_layout(G)
+# Vẽ đồ thị
+pos = nx.spring_layout(G)  # Sử dụng spring layout
 nx.draw(G, pos, with_labels=True, node_size=3000, node_color="skyblue", font_size=20)
 
 # Hiển thị trọng số trên các cạnh
 labels = nx.get_edge_attributes(G, 'weight')
 nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
 
+# Hiển thị đồ thị
 plt.show()
